04d7432bab0de4c6e699942db8ade0bcbec3fa6b
konstantinosKokos <konstantinos@riseup.net>
2018-10-28 13:34:38 +0100

    prework for dataloading

--- utils/SeqUtils.py
+++ utils/SeqUtils.py
@@ -1,6 +1,10 @@
 from itertools import chain
 import pickle
-from utils import fastText
+from utils import FastText
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import Compose
 
 
 def load():
@@ -20,10 +24,10 @@ def make_oov_files(input_file='test-output/sequences/words-types.p', iv_vectors_
 
     all_words = get_all_unique(ws)
 
-    model_vectors = fastText.load_vectors(iv_vectors_file)
-    oov_words = fastText.find_oov(all_words, model_vectors)
-    fastText.write_oov(oov_words, oov_words_file=oov_words_file)
-    fastText.vectorize_oov(oov_words_file, oov_vectors_file)
+    model_vectors = FastText.load_vectors(iv_vectors_file)
+    oov_words = FastText.find_oov(all_words, model_vectors)
+    FastText.write_oov(oov_words, oov_words_file=oov_words_file)
+    FastText.vectorize_oov(oov_words_file, oov_vectors_file)
 
 
 def get_type_occurrences(type_sequences):
@@ -51,7 +55,59 @@ def get_high_occurrence_sequences(word_sequences, type_sequences, threshold):
     return [word_sequences[i] for i in kept_indices], [type_sequences[i] for i in kept_indices]
 
 
-# todo: vectorize word sequences
+def get_low_len_sequences(word_sequences, type_sequences, max_len):
+    """
+
+    :param word_sequences:
+    :param type_sequences:
+    :param max_len:
+    :return:
+    """
+    assert len(word_sequences) == len(type_sequences)
+    kept_indices = [i for i in range(len(word_sequences)) if len(word_sequences[i]) <= max_len]
+    return [word_sequences[i] for i in kept_indices], [type_sequences[i] for i in kept_indices]
+
+
+def map_ws_to_vs(word_sequence, vectors):
+    # seq_len x num_features x single batch
+    return torch.Tensor(list(map(lambda x: vectors[x], word_sequence))).view(-1, 300, 1)
+
+
+class Sequencer(Dataset):
+    def __init__(self, word_sequences, type_sequences, vectors, max_sentence_length=None,
+                 minimum_type_occurrence=None, transform=None):
+        """
+        Necessary in the case of larger data sets that cannot be stored in memory.
+        :param word_sequences:
+        :param type_sequences:
+        :param vectors:
+        :param max_sentence_length:
+        :param minimum_type_occurrence:
+        :param transform:
+        """
+        super(self, Sequencer)
+        if max_sentence_length:
+            word_sequences, type_sequences = get_low_len_sequences(word_sequences, type_sequences, max_sentence_length)
+        if minimum_type_occurrence:
+            word_sequences, type_sequences = get_high_occurrence_sequences(word_sequences, type_sequences,
+                                                                           minimum_type_occurrence)
+        self.transform = transform
+        self.word_sequences = word_sequences
+        self.type_sequences = type_sequences
+        self.max_sentence_length = max_sentence_length
+        self.types = {t: i+1 for i, t in enumerate(get_all_unique(type_sequences))}
+        self.vectors = vectors
+        assert len(word_sequences) == len(type_sequences)
+        self.len = len(word_sequences)
+
+    def __len__(self):
+        return self.__len__()
+
+    def __getitem__(self, item):
+        if self.transform:
+            return self.transform(self.word_sequences[item], self.type_sequences[item])
+        else:
+            return self.word_sequences[item], self.type_sequences[item]
 
 # todo: one-hot type sequences
 
