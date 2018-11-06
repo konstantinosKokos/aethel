1faf1e9266bdbf266a92cb1b5f16dd414dd72b62
konstantinosKokos <konstantinos@riseup.net>
2018-11-06 22:56:57 +0100

    refactor WordType

--- WordType.py
+++ WordType.py
@@ -1,174 +1,207 @@
 from itertools import permutations
+from warnings import warn
+from functools import reduce
+
 
 class WordType:
     def __init__(self, arglist, result, modality=False):
-        # make sure that [] -> A -> B is the same as A -> B
-        while type(result) == WordType:
+        """
+        Constructor for WordType.
+        :param arglist:
+        :param result:
+        :param modality:
+        """
+
+        # normalize the arglist
+        if isinstance(arglist, str):
+            # if str, assume singular type argument and convert it to tuple of WordType
+            arglist = (WordType((), arglist),)
+            warn('Implicit conversion from str to WordType.')
+        elif isinstance(arglist, list) or isinstance(arglist, tuple):
+            # if iterable, assert all items are WordTypes (also covers the case of empty iterable)
+            if not all(map(lambda t: isinstance(t, WordType), arglist)):
+                raise TypeError('Expected an iterable of WordTypes, received {} instead.'.
+                                format(list(map(type, arglist))))
+            arglist = tuple(arglist)
+        elif isinstance(arglist, WordType):
+            # if WordType, convert to tuple of WordType
+            arglist = (arglist,)
+
+        # normalize the result
+        if isinstance(result, str):
+            # if str, simply store it
+            pass
+        elif isinstance(result, list) or isinstance(result, tuple):
+            # if iterable, try to convert to WordType
+            if len(result) == 1:
+                if isinstance(result[0], WordType):
+                    self.result = result[0]
+                    warn('Implicit conversion from iterable to WordType.')
+                else:
+                    raise TypeError('Expected result type to be str or Wordtype, received {} instead.'
+                                    .format(type(result)))
+            else:
+                raise TypeError('A WordType cannot have a sequence as a result.')
+        elif not isinstance(result, WordType):
+            raise TypeError('WordType result must be str or WordType, received {} instead.'.format(type(result)))
+
+        while isinstance(result, WordType):
+            # final sanity check
             if not result.arglist:
+                # rxemove empty internal arglist
                 result = result.result
             elif not arglist:
+                # remove empty external arglist
                 arglist = result.arglist
                 result = result.result
             else:
                 break
+
         self.arglist = arglist
         self.result = result
         self.modality = modality
-
-        # todo: arglist arity is ignored
-        if type(self.result) == WordType:
-            result_arity = result.arity
-        else:
-            result_arity = 0
-        if not arglist:
-            self.arity = result_arity
-        else:
-            arglist_arity = self.find_arglist_arity(arglist)
-            self.arity = result_arity + arglist_arity + 1
-
-    def find_arglist_arity(self, arglist):
-        arglist_arity = 0
-        for a in arglist:
-            if type(a) == WordType:
-                arglist_arity = max(arglist_arity, a.arity)
-        return arglist_arity
+        self.arity = WordType.get_arity(result) if not self.arglist else self.find_arglist_arity() + 1 + \
+                                                                WordType.get_arity(self.result)
 
     @staticmethod
-    def print_arg(arg):
-        if type(arg) == list:  # case of item w/ dep
-            return '(' + arg[0].__str__() + ', ' + arg[1] + ')'
-        elif type(arg) == str:  # case of item w/o dep
-            return arg
+    def get_arity(item):
+        if isinstance(item, str):
+            return 0
+        elif isinstance(item, WordType):
+            return item.arity
         else:
-            raise TypeError('Unknown type')
+            raise TypeError('Unknown type.')
+
+    def find_arglist_arity(self):
+        return reduce(max, map(lambda x: x.arity, [a for a in self.arglist if isinstance(a, WordType)]), 0)
 
     def __str__(self):
-        to_print = ''
-        if self.arglist:
-            # len check for parentheses
-            if len(self.arglist) == 1:  # single item with dep
-                argprint = self.print_arg(self.arglist[0])
-            else:
-                argprint = '(' + ', '.join(map(self.print_arg, self.arglist)) + ')'
-            to_print = argprint + ' → '
-        resprint = str(self.result)
-        try:
-            if self.result.arity:
-                resprint = '(' + resprint + ')'
-        except AttributeError:
-            pass
-        to_print = to_print + resprint
-        if self.modality:
-            if self.arglist:
-                to_print = '(' + to_print + ')'
-            to_print = ' ◇ □ ' + to_print
-        return to_print
+        """
+        Conversion to string
+        :return:
+        """
+        def print_args(args):
+            if isinstance(args, tuple) or isinstance(args, list):
+                if len(args) > 1:
+                    return '(' + ', '.join([a.__str__() for a in args]) + ') → '
+                elif len(args) == 1:
+                    if args[0].arity == 0:
+                        return args[0].__str__() + ' → '
+                    else:
+                        return '(' + args[0].__str__() + ') → '
+                else:
+                    return ''
+            elif isinstance(args, WordType):
+                return args.__str__()
+        if isinstance(self.result, WordType):
+            s = print_args(self.arglist) + self.result.__str__()
+            return '!' + s if self.modality else s
+        elif isinstance(self.result, str):
+            s = print_args(self.arglist) + self.result
+            return '!' + s if self.modality else s
 
     def __repr__(self):
         return self.__str__()
 
-    def __eq__(self, t):
-        # two wordtypes can never be the same if their word types do not match
-        if type(self) != type(t):
-            return False
-        # recursive calls within argument and result types
-        return self.arglist == t.arglist and self.result == t.result and self.modality == t.modality
+    def __call__(self):
+        return self.__str__()
 
     def __hash__(self):
-        # hash the string representation which should be unique for any given type
         return self.__repr__().__hash__()
 
-    def wcmp(self, other):
-        """
-        Weak comparison between a normal Type and a Type with a missing argument / result
-        :param other:
-        :return:
-        """
-        # fuck python
-        if len(self.arglist) != len(other.arglist):
-            return False
-        for i, a in enumerate(self.arglist):
-            if a == other.arglist[i] or (other.arglist[i][0] == '?' and a[1] == other.arglist[i][1]):
-                continue
-            else:
-                return False
-        # if other.result == '?': # todo
-        #     return True
-        if type(self.result)!=type(other.result):
-            print(1)
-            return False
-        elif type(self.result) == WordType:
-            return True and self.result.wcmp(other.result)
-        else:
-            return self.result == other.result
+    def __eq__(self, other):
+        if isinstance(other, WordType):
+            return self.arglist == other.arglist and self.result == other.result and self.modality == other.modality
+        return False
 
-    @staticmethod
-    def remove_deps(wordtype):
-        arglist = []
-        for a in wordtype.arglist:
-            if type(a) == WordType:
-                arglist.append(WordType.remove_deps(a).__str__())
-            else:
-                arglist.append(a[0].__str__())
-        if type(wordtype.result) == WordType:
-            result = WordType.remove_deps(wordtype.result)
-        else:
-            result = wordtype.result
-        return WordType(arglist, result, wordtype.modality)
-
-    def __main__(self):
-        print(self.__str__)
-
-
-def collapse(seq, verbose=False):
-    # todo: intractable
-    def apply(t1, t2):
-        r = apply_left(t1, t2)
-        if r:
-            return r
-        else:
-            return apply_left(t2, t1)
-
-    def apply_left(t1, t2):
-        if not t1.arglist and t1.result in t2.arglist:
-            if isinstance(t2.arglist, list):
-                return WordType([x for x in t2.arglist if x != t1.result], t2.result)
-            else:
-                return WordType([], t2.result)
-        return None
-
-    perms = permutations(seq)  # take all permutations of the given type sequence
-    perms_ns = perms
-    # for p in perms:
-    #     if p[::-1] not in perms_ns:
-    #         perms_ns.append(p)  # ignore the symmetric ones
-
-    precomputed = dict()
-
-    for p in perms_ns:
-        current = list(p)
-        reduced = True
-        while reduced:
-            if verbose:
-                print('starting with :', current)
-            reduced = False
-            if len(current) == 1:
-                return current
-            for t1, t2 in zip(current, current[1:]):
-                if tuple([t1, t2]) in precomputed.keys():
-                    t12 = precomputed[tuple([t1, t2])]
-                else:
-                    t12 = apply(t1, t2)
-                    precomputed[tuple([t1, t2])] = t12
-                    precomputed[tuple([t2, t1])] = t12
-                if t12:
-                    if verbose:
-                        print('removing ', t1, t2, 'and replacing with ', t12)
-                    current.remove(t1)
-                    current.remove(t2)
-                    current.append(t12)
-                    reduced = True
-                    break
-            if reduced==False and verbose:
-                print('Failed.')
-    return None
+#     def wcmp(self, other):
+#         """
+#         Weak comparison between a normal Type and a Type with a missing argument / result
+#         :param other:
+#         :return:
+#         """
+#         # fuck python
+#         if len(self.arglist) != len(other.arglist):
+#             return False
+#         for i, a in enumerate(self.arglist):
+#             if a == other.arglist[i] or (other.arglist[i][0] == '?' and a[1] == other.arglist[i][1]):
+#                 continue
+#             else:
+#                 return False
+#         # if other.result == '?': # todo
+#         #     return True
+#         if type(self.result)!=type(other.result):
+#             print(1)
+#             return False
+#         elif type(self.result) == WordType:
+#             return True and self.result.wcmp(other.result)
+#         else:
+#             return self.result == other.result
+#
+#     @staticmethod
+#     def remove_deps(wordtype):
+#         arglist = []
+#         for a in wordtype.arglist:
+#             if type(a) == WordType:
+#                 arglist.append(WordType.remove_deps(a).__str__())
+#             else:
+#                 arglist.append(a[0].__str__())
+#         if type(wordtype.result) == WordType:
+#             result = WordType.remove_deps(wordtype.result)
+#         else:
+#             result = wordtype.result
+#         return WordType(arglist, result, wordtype.modality)
+#
+#
+# def collapse(seq, verbose=False):
+#     # todo: intractable
+#     def apply(t1, t2):
+#         r = apply_left(t1, t2)
+#         if r:
+#             return r
+#         else:
+#             return apply_left(t2, t1)
+#
+#     def apply_left(t1, t2):
+#         if not t1.arglist and t1.result in t2.arglist:
+#             if isinstance(t2.arglist, list):
+#                 return WordType([x for x in t2.arglist if x != t1.result], t2.result)
+#             else:
+#                 return WordType([], t2.result)
+#         return None
+#
+#     perms = permutations(seq)  # take all permutations of the given type sequence
+#     perms_ns = perms
+#     # for p in perms:
+#     #     if p[::-1] not in perms_ns:
+#     #         perms_ns.append(p)  # ignore the symmetric ones
+#
+#     precomputed = dict()
+#
+#     for p in perms_ns:
+#         current = list(p)
+#         reduced = True
+#         while reduced:
+#             if verbose:
+#                 print('starting with :', current)
+#             reduced = False
+#             if len(current) == 1:
+#                 return current
+#             for t1, t2 in zip(current, current[1:]):
+#                 if tuple([t1, t2]) in precomputed.keys():
+#                     t12 = precomputed[tuple([t1, t2])]
+#                 else:
+#                     t12 = apply(t1, t2)
+#                     precomputed[tuple([t1, t2])] = t12
+#                     precomputed[tuple([t2, t1])] = t12
+#                 if t12:
+#                     if verbose:
+#                         print('removing ', t1, t2, 'and replacing with ', t12)
+#                     current.remove(t1)
+#                     current.remove(t2)
+#                     current.append(t12)
+#                     reduced = True
+#                     break
+#             if reduced==False and verbose:
+#                 print('Failed.')
+#     return None
