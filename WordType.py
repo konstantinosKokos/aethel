6b81a3193e1ad2f5caa7bfc4514930bd317b18a0
konstantinosKokos <konstantinos@riseup.net>
2018-11-21 16:06:04 +0100

    CombinatorType

--- WordType.py
+++ WordType.py
@@ -96,7 +96,7 @@ class ComplexType(WordType):
         if not isinstance(result, WordType):
             raise TypeError('Expected result to be of type WordType, received {} instead.'.format(type(result)))
         self.result = result
-        if not isinstance(arguments, list) and not isinstance(arguments, tuple):
+        if not isinstance(arguments, tuple):
             raise TypeError('Expected arguments to be a tuple of WordTypes, received {} instead.'.
                             format(type(arguments)))
         if not all(map(lambda x: isinstance(x, WordType), arguments)) or len(arguments) == 0:
@@ -131,12 +131,11 @@ class ComplexType(WordType):
 
 class ColoredType(ComplexType):
     def __init__(self, arguments, result, colors):
-        if not isinstance(colors, list) and not isinstance(colors, tuple):
+        if not isinstance(colors, tuple):
             raise TypeError('Expected color to be  a tuple of strings, received {} instead.'.format(type(colors)))
         if not all(map(lambda x: isinstance(x, str), colors)) or len(colors) == 0:
             raise TypeError('Expected arguments to be a non-empty tuple of strings,'
-                            ' received a tuple containing {} instead.'.
-                            format(list(map(type, colors))))
+                            ' received a tuple containing {} instead.'.format(list(map(type, colors))))
         if len(colors) != len(arguments):
             raise ValueError('Uneven amount of arguments ({}) and colors ({}).'.format(len(arguments), len(colors)))
         sorted_ac = sorted([ac for ac in zip(arguments, colors)], key=lambda x: x[0].__repr__() + x[1].__repr__())
@@ -162,6 +161,39 @@ class ColoredType(ComplexType):
             return False
         else:
             return (self.arguments, self.colors) == (other.arguments, other.colors) and self.result == other.result
+
+class CombinatorType(WordType):
+    def __init__(self, types, combinator):
+        if not isinstance(types, tuple):
+            raise TypeError('Expected types to be  a tuple of WordTypes, received {} instead.'.format(type(types)))
+        if not all(map(lambda x: isinstance(x, WordType)), types) or len(types) < 1:
+            raise TypeError('Expected types to be a non-empty tuple of WordTypes,'
+                            ' received a tuple containing {} instead.'.format(list(map(type, types))))
+        if not isinstance(combinator, str):
+            raise TypeError('Expected combinator to be of type str, received {} instead.'.format(type(combinator)))
+        self.types = sorted(types, key=lambda x: x.__repr__())
+        self.combinator = combinator
+
+    def __str__(self):
+        return (' ' + self.combinator + ' ').join(t.__repr__() for t in self.types)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __hash__(self):
+        return self.__str__().__hash__()
+
+    def get_arity(self):
+        return max(t.get_arity() for t in self.types)
+
+    def __call__(self):
+        return self.__str__()
+
+    @abstractmethod
+    def __eq__(self, other):
+        if isinstance(other, CombinatorType) and self.types == other.types and self.combinator == other.combinator:
+                return True
+        return False
 
 
 def compose(base_types, base_colors, result):
