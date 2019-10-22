bd31a53bb7e93b9aa080641fd6cdc2ea13ec6f3b
konstantinosKokos <konstantinos@riseup.net>
2019-10-22 14:36:32 +0200

    compose

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from itertools import chain
 
-from src.extraction import Extraction, _cat_dict, _pt_dict, _head_deps, _mod_deps
+from src.extraction import Extraction, _cat_dict, _pt_dict, _head_deps, _mod_deps, ExtractionError
 from src.lassy import Lassy
 from src.proofs import Prove
 from src.transformations import Transformation
 from src.viz import ToGraphViz
+
+from typing import Optional, Union, Tuple, List
+from src.graphutils import DAG
+from src.proofs import ProofNet
 
 _lassy = Lassy()
 _transform = Transformation()
@@ -13,14 +17,23 @@ _prove = Prove()
 _viz = ToGraphViz()
 
 
-def test(samples=100):
-    meta = [{'src': _lassy[i][1]} for i in range(samples)]
+def test(size, start=0):
+    meta = [{'src': _lassy[i][1]} for i in range(start, start+size)]
 
     transformed = list(chain.from_iterable(list(map(_transform,
-                                                    list(map(lambda i: _lassy[i][2], range(samples))), meta))))
+                                                    list(map(lambda i: _lassy[i][2], range(start, start+size))),
+                                                    meta))))
     transformed = list(filter(lambda t: t is not None, transformed))
+
     extracted = list(map(_extract, transformed))
     extracted = list(filter(lambda e: e is not None, extracted))
     return extracted
     # prover = Prover(extracted, _prove)
     # return prover
+
+
+def compose(sample: Union[int, str]) -> List[Optional[Tuple[ProofNet, DAG]]]:
+    dags = _transform(_lassy[sample][2])
+    extracted = list(filter(lambda e: e is not None, list(map(_extract, dags))))
+    proven = list(map(_prove, extracted))
+    return proven
