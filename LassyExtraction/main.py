bec40b44b595a8776abdac619b4d6b9e7b058a4e
konstantinosKokos <konstantinos@riseup.net>
2020-03-05 14:29:27 +0100

    minor

--- LassyExtraction/main.py
+++ LassyExtraction/main.py
@@ -2,13 +2,14 @@ from itertools import chain
 
 from LassyExtraction.extraction import extractor
 from LassyExtraction.lassy import Lassy
-from LassyExtraction.proofs import prover
+from LassyExtraction.proofs import prover, ProofNet
 from LassyExtraction.transformations import transformer
+from LassyExtraction.graphutils import DAG
 from LassyExtraction.viz import ToGraphViz
 
 from typing import Optional, Union, Tuple, List
-from LassyExtraction.graphutils import DAG
-from LassyExtraction.proofs import ProofNet
+
+from tqdm import tqdm
 
 _lassy = Lassy()
 _viz = ToGraphViz()
@@ -17,9 +18,9 @@ _viz = ToGraphViz()
 def compose(sample: Union[int, str]) -> List[Optional[Tuple[ProofNet, DAG]]]:
     dags = transformer(_lassy[sample][2], meta={'src': _lassy[sample][1]})
     extracted = list(filter(lambda e: e is not None, list(map(extractor, dags))))
-    proven = list(map(lambda e: (e, prover(e, raise_errors=False)), extracted))
+    proven = list(map(lambda e: prover(e, raise_errors=False), extracted))
     return proven
 
 
 def exhaust():
-    return list(chain.from_iterable(map(lambda i: compose(i), range(len(_lassy)))))
+    return list(chain.from_iterable(map(lambda i: compose(i), tqdm(range(len(_lassy))))))
