8c4c24f8d576da799f843400bc0004e5a0cce74e
konstantinosKokos <konstantinos@riseup.net>
2020-03-06 13:13:43 +0100

    convenience tools

---
+++ LassyExtraction/utils/tools.py
@@ -0,0 +1,8 @@
+from LassyExtraction.transformations import order_nodes
+from LassyExtraction.graphutils import DAG
+from LassyExtraction.transformations import get_sentence as get_words
+from LassyExtraction.milltypes import WordTypes
+
+
+def get_types(dag: DAG) -> WordTypes:
+    return list(map(lambda leaf: dag.attribs[leaf]['type'], list(dag.get_leaves())))
