410b0806fdb21c0d35ded8003f23b66c31f0a5b7
konstantinosKokos <konstantinos@riseup.net>
2020-04-17 11:24:21 +0200

    sloppy lambda conversion

---
+++ LassyExtraction/lambdas.py
@@ -0,0 +1,152 @@
+from LassyExtraction.milltypes import *
+from typing import *
+from dataclasses import dataclass
+
+Links = Dict[str, str]
+
+SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+
+
+def translate(idx: int) -> str:
+    return str(idx).translate(SUB)
+
+
+@dataclass
+class Node:
+    idx: str
+    name: str
+    polarity: bool
+    terminal: bool
+
+    def __init__(self, idx: str, name: str, polarity: bool, terminal: bool):
+        self.idx = idx
+        self.name = name
+        self.polarity = polarity
+        self.terminal = terminal
+
+    def __hash__(self):
+        return (self.idx, self.name, self.polarity).__hash__()
+
+
+class Graph(object):
+    def __init__(self):
+        self.nodes = set()
+        self.edges = set()
+
+    def add_intra_graphs(self, words: strings, types: WordTypes):
+        nodes, edges = list(zip(*list(map(make_intra_graphs, words, types))))
+        self.nodes.update(set.union(*nodes))
+        self.edges.update(set.union(*edges))
+
+    def add_inter_graphs(self, proofnet: Dict[int, int]):
+        self.edges.update({(str(k), str(v)) for k, v in proofnet.items()})
+
+    def get_node(self, idx: str) -> Node:
+        nodes = list(filter(lambda node: node.idx == idx, self.nodes))
+        assert len(nodes) == 1
+        return nodes[0]
+
+    def downward(self, node: Node) -> Set[Node]:
+        node_ids = set(map(lambda edge: edge[0], filter(lambda edge: edge[1] == node.idx, self.edges)))
+        nodes = set(filter(lambda other: other.idx in node_ids, self.nodes))
+        return nodes
+
+    def upward(self, node: Node) -> Set[Node]:
+        node_ids = set(map(lambda edge: edge[1], filter(lambda edge: edge[0] == node.idx, self.edges)))
+        nodes = set(filter(lambda other: other.idx in node_ids, self.nodes))
+        return nodes
+
+    def to_input(self, node: Node) -> Node:
+        downward = self.downward(node)
+        downward_input = list(filter(lambda other: other.polarity, downward))
+        assert len(downward_input) == 1
+        return downward_input[0]
+
+    def to_output(self, node: Node) -> Node:
+        upward = self.upward(node)
+        upward_output = list(filter(lambda other: not other.polarity, upward))
+        assert len(upward_output) == 1
+        return upward_output[0]
+
+
+def make_graph(words: strings, premises: WordTypes, conclusion: WordType) -> Graph:
+    graph = Graph()
+    graph.add_intra_graphs(words, premises + [conclusion])
+    return graph
+
+
+def get_type_indices(wordtype: WordType) -> List[int]:
+    pos, neg = get_polarities_and_indices(wordtype)
+    return sorted(reduce(add, map(lambda x: [x[1]], pos), []) + reduce(add, map(lambda x: [x[1]], neg), []))
+
+
+def make_atomic_nodes(types: WordTypes) -> Set[AtomicType]:
+    return set.union(*list(map(get_atomic, types)))
+
+
+def make_intra_graphs(word: str, wordtype: WordType, polarity: bool = True, parent: Optional[str] = None) \
+        -> Tuple[Set[Node], Set[Tuple[str, str]]]:
+    if isinstance(wordtype, AtomicType):
+        return ({Node(idx=str(wordtype.index), polarity=wordtype.polarity, name=word, terminal=True)},
+                {(parent, str(wordtype.index))} if parent is not None else set())
+    else:
+        # get identifiers for each subtree
+        left_id = '.'.join(list(map(str, get_type_indices(wordtype.argument))))
+        right_id = '.'.join(list(map(str, get_type_indices(wordtype.result))))
+        # init the implication node
+        node = Node(idx=f'{left_id} | {right_id}', polarity=polarity, name=word, terminal=False)
+        edge = {(parent, node.idx)} if parent is not None else set()
+        current_decoration = get_decoration(wordtype)
+        left_nodes, left_edges = make_intra_graphs(word, wordtype.argument, not polarity, node.idx)
+        right_nodes, right_edges = make_intra_graphs(word, wordtype.result, polarity, node.idx)
+        return {node}.union(left_nodes).union(right_nodes), edge.union(left_edges).union(right_edges)
+
+
+def get_decoration(functor: FunctorType):
+    return functor.diamond if isinstance(functor, DiamondType) else functor.box if isinstance(functor, BoxType) else '→'
+
+
+def traverse(graph: Graph, idx: str, forward_dict: Links, backward_dict: Links, upward: bool,
+             varcount: int) -> Tuple[str, int]:
+    node = graph.get_node(idx)
+    if upward:
+        # leaf case, cross to input
+        if node.terminal:
+            ret, varcount = traverse(graph, backward_dict[idx], forward_dict, backward_dict, not upward, varcount)
+            return ret, varcount
+        else:
+            ret = f'λx{translate(varcount)}.'
+            varcount += 1
+            ret2, varcount = traverse(graph, graph.to_output(node).idx, forward_dict, backward_dict, upward,
+                                      varcount)
+            return ret+ret2, varcount
+    else:
+        # root case
+        if not graph.downward(node):
+            # terminal root case
+            if node.terminal:
+                ret = f'{node.name}'
+                return ret, varcount
+            else:
+                # root impl case, move to other branch, switch mode
+                if node.polarity:
+                    ret = f'({node.name} '
+                    ret2, varcount = traverse(graph, graph.to_output(node).idx, forward_dict, backward_dict, not upward,
+                                              varcount)
+                    return ret+f'{ret2}) ', varcount
+                else:
+                    raise NotImplementedError
+        else:
+            if node.terminal:
+                try:
+                    ret, varcount = traverse(graph, graph.to_input(node).idx, forward_dict, backward_dict, upward, varcount)
+                    return ret, varcount
+                except AssertionError:
+                    ret = f'x{translate(varcount-1)}'
+                    return ret, varcount
+            else:
+                ret2, varcount = traverse(graph, graph.to_input(node).idx, forward_dict, backward_dict, upward, varcount)
+                ret3, varcount = traverse(graph, graph.to_output(node).idx, forward_dict, backward_dict, not upward,
+                                          varcount)
+                return f'({ret2+ret3})', varcount
+
--- LassyExtraction/utils/tools.py
+++ LassyExtraction/utils/tools.py
@@ -1,8 +1,43 @@
 from LassyExtraction.transformations import order_nodes
 from LassyExtraction.graphutils import DAG
 from LassyExtraction.transformations import get_sentence as get_words
-from LassyExtraction.milltypes import WordTypes
+from LassyExtraction.milltypes import WordTypes, AtomicType, get_polarities_and_indices, PolarizedType
+from LassyExtraction.lambdas import make_graph as _make_graph, traverse
+
+from typing import List, Tuple, Dict
+
+from functools import reduce
+from operator import add
+
+Proof = Dict[int, int]
 
 
 def get_types(dag: DAG) -> WordTypes:
     return list(map(lambda leaf: dag.attribs[leaf]['type'], list(order_nodes(dag, list(dag.get_leaves())))))
+
+
+def get_conclusion(_atoms: List[Tuple[AtomicType, int]], _proof: Proof) -> Tuple[AtomicType, int]:
+    antecedents = set(map(lambda x: x[1], _atoms))
+    conclusion_id = list(set(map(lambda x: x[1],
+                                 _proof)).
+                         union(set(map(lambda x: x[0],
+                                       _proof))).
+                         difference(antecedents))[0]
+    conclusion_pair = list(filter(lambda pair: pair[1] == conclusion_id, _proof))[0][0]
+    conclusion_atom = list(filter(lambda a: a[1] == conclusion_pair, _atoms))[0][0]
+    return conclusion_atom, conclusion_id
+
+
+def make_graph(dag: DAG, proof: Dict[int, int]) -> str:
+    words = get_words(dag) + ['conc']
+    if len(words) == 2:
+        return words[0]
+    types = get_types(dag)
+    atoms = list(zip(*list(map(get_polarities_and_indices, types))))
+    negative, positive = list(map(lambda x: reduce(add, x), atoms))
+    conclusion, conclusion_id = get_conclusion(_atoms=negative + positive, _proof=proof)
+    conclusion = PolarizedType(wordtype=conclusion.type, polarity=False, index=conclusion_id)
+    graph = _make_graph(words, types, conclusion)
+    the_lambda = traverse(graph, str(conclusion_id), {str(k): str(v) for k, v in proof},
+                          {str(v): str(k) for k, v in proof}, True, 0)[0]
+    return the_lambda
