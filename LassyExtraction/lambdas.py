9e002c37c6eb502628ab054e4fa4b48cd430eab8
konstantinosKokos <konstantinos@riseup.net>
2020-04-23 14:39:23 +0200

    add type hints in λ terms

--- LassyExtraction/lambdas.py
+++ LassyExtraction/lambdas.py
@@ -2,13 +2,24 @@ from LassyExtraction.milltypes import *
 from typing import *
 from dataclasses import dataclass
 
-Links = Dict[str, str]
+IntMapping = Dict[int, int]
+StrMapping = Dict[str, str]
 
-SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+SUB = str.maketrans('0123456789', '₀₁₂₃₄₅₆₇₈₉')
+SUP = str.maketrans('abcdefghijklmnoprstuvwxyz1', 'ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖʳˢᵗᵘᵛʷˣʸᶻ¹')
+TYPES = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZ1→', 'ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ1→')
 
 
-def translate(idx: int) -> str:
+def translate_id(idx: int) -> str:
     return str(idx).translate(SUB)
+
+
+def translate_decoration(decoration: str) -> str:
+    return decoration.lower().translate(SUP)
+
+
+def translate_type(wordtype: WordType) -> str:
+    return str(wordtype).upper().translate(TYPES)
 
 
 @dataclass
@@ -17,12 +28,14 @@ class Node:
     name: str
     polarity: bool
     terminal: bool
+    decoration: str = ''
 
-    def __init__(self, idx: str, name: str, polarity: bool, terminal: bool):
+    def __init__(self, idx: str, name: str, polarity: bool, terminal: bool, decoration: str = ''):
         self.idx = idx
         self.name = name
         self.polarity = polarity
         self.terminal = terminal
+        self.decoration = decoration
 
     def __hash__(self):
         return (self.idx, self.name, self.polarity).__hash__()
@@ -38,7 +51,7 @@ class Graph(object):
         self.nodes.update(set.union(*nodes))
         self.edges.update(set.union(*edges))
 
-    def add_inter_graphs(self, proofnet: Dict[int, int]):
+    def add_inter_graphs(self, proofnet: IntMapping):
         self.edges.update({(str(k), str(v)) for k, v in proofnet.items()})
 
     def get_node(self, idx: str) -> Node:
@@ -71,6 +84,10 @@ class Graph(object):
 
 def make_graph(words: strings, premises: WordTypes, conclusion: WordType) -> Graph:
     graph = Graph()
+    words = list(map(lambda w, t:
+                     f'{w}::{translate_type(t.depolarize().decolor())}',
+                     words,
+                     premises + [conclusion]))
     graph.add_intra_graphs(words, premises + [conclusion])
     return graph
 
@@ -94,9 +111,9 @@ def make_intra_graphs(word: str, wordtype: WordType, polarity: bool = True, pare
         left_id = '.'.join(list(map(str, get_type_indices(wordtype.argument))))
         right_id = '.'.join(list(map(str, get_type_indices(wordtype.result))))
         # init the implication node
-        node = Node(idx=f'{left_id} | {right_id}', polarity=polarity, name=word, terminal=False)
+        node = Node(idx=f'{left_id} | {right_id}', polarity=polarity, name=word, terminal=False,
+                    decoration=get_decoration(wordtype))
         edge = {(parent, node.idx)} if parent is not None else set()
-        current_decoration = get_decoration(wordtype)
         left_nodes, left_edges = make_intra_graphs(word, wordtype.argument, not polarity, node.idx)
         right_nodes, right_edges = make_intra_graphs(word, wordtype.result, polarity, node.idx)
         return {node}.union(left_nodes).union(right_nodes), edge.union(left_edges).union(right_edges)
@@ -106,7 +123,7 @@ def get_decoration(functor: FunctorType):
     return functor.diamond if isinstance(functor, DiamondType) else functor.box if isinstance(functor, BoxType) else '→'
 
 
-def traverse(graph: Graph, idx: str, forward_dict: Links, backward_dict: Links, upward: bool,
+def traverse(graph: Graph, idx: str, forward_dict: StrMapping, backward_dict: StrMapping, upward: bool,
              varcount: int) -> Tuple[str, int]:
     node = graph.get_node(idx)
     if upward:
@@ -115,38 +132,47 @@ def traverse(graph: Graph, idx: str, forward_dict: Links, backward_dict: Links, 
             ret, varcount = traverse(graph, backward_dict[idx], forward_dict, backward_dict, not upward, varcount)
             return ret, varcount
         else:
-            ret = f'λx{translate(varcount)}.'
+            ret = f'λx{translate_id(varcount)}.'
             varcount += 1
-            ret2, varcount = traverse(graph, graph.to_output(node).idx, forward_dict, backward_dict, upward,
-                                      varcount)
+            ret2, varcount = traverse(graph, graph.to_output(node).idx, forward_dict,
+                                      backward_dict, upward, varcount)
             return ret+ret2, varcount
     else:
         # root case
         if not graph.downward(node):
             # terminal root case
             if node.terminal:
-                ret = f'{node.name}'
+                ret = f' {node.name}'
                 return ret, varcount
             else:
                 # root impl case, move to other branch, switch mode
                 if node.polarity:
-                    ret = f'({node.name} '
-                    ret2, varcount = traverse(graph, graph.to_output(node).idx, forward_dict, backward_dict, not upward,
-                                              varcount)
-                    return ret+f'{ret2}) ', varcount
+                    ret = f'({node.name}'
+                    ret2, varcount = traverse(graph, graph.to_output(node).idx, forward_dict,
+                                              backward_dict, not upward, varcount)
+                    if node.decoration in {'mod', 'app', 'predm', 'det'}:
+                        return f'{ret}{translate_decoration(node.decoration)} {ret2})', varcount
+                    else:
+                        return f'{ret} {ret2}{translate_decoration(node.decoration)})', varcount
                 else:
                     raise NotImplementedError
         else:
             if node.terminal:
                 try:
-                    ret, varcount = traverse(graph, graph.to_input(node).idx, forward_dict, backward_dict, upward, varcount)
+                    ret, varcount = traverse(graph, graph.to_input(node).idx, forward_dict,
+                                             backward_dict, upward, varcount)
                     return ret, varcount
                 except AssertionError:
-                    ret = f'x{translate(varcount-1)}'
+                    ret = f'x{translate_id(varcount - 1)}'
                     return ret, varcount
             else:
-                ret2, varcount = traverse(graph, graph.to_input(node).idx, forward_dict, backward_dict, upward, varcount)
-                ret3, varcount = traverse(graph, graph.to_output(node).idx, forward_dict, backward_dict, not upward,
-                                          varcount)
-                return f'({ret2+ret3})', varcount
+                ret2, varcount = traverse(graph, graph.to_input(node).idx, forward_dict,
+                                          backward_dict, upward, varcount)
+                ret3, varcount = traverse(graph, graph.to_output(node).idx, forward_dict,
+                                          backward_dict, not upward, varcount)
+                if node.decoration in {'mod', 'app', 'predm', 'det'}:
+                    ret2 += translate_decoration(node.decoration)
+                else:
+                    ret3 += translate_decoration(node.decoration)
+                return f'({ret2} {ret3})', varcount
 
