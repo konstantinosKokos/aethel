7c8698afc25fd557ecacb9fe457026202ab08d01
konstantinosKokos <konstantinos@riseup.net>
2020-12-02 17:13:58 +0100

    remove tqdm dep

--- LassyExtraction/main.py
+++ LassyExtraction/main.py
@@ -2,20 +2,19 @@ from itertools import chain
 
 from LassyExtraction.extraction import extractor
 from LassyExtraction.lassy import Lassy
-from LassyExtraction.proofs import prover, ProofNet
+from LassyExtraction.proofs import prover, AxiomLinks
 from LassyExtraction.transformations import transformer
 from LassyExtraction.graphutils import DAG
 from LassyExtraction.viz import ToGraphViz
 
 from typing import Optional, Union, Tuple, List
 
-from tqdm import tqdm
 
 _lassy = Lassy()
 _viz = ToGraphViz()
 
 
-def compose(sample: Union[int, str]) -> List[Optional[Tuple[ProofNet, DAG]]]:
+def compose(sample: Union[int, str]) -> List[Optional[Tuple[AxiomLinks, DAG]]]:
     dags = transformer(_lassy[sample][2], meta={'src': _lassy[sample][1]})
     extracted = list(filter(lambda e: e is not None, list(map(extractor, dags))))
     proven = list(map(lambda e: prover(e, raise_errors=False), extracted))
@@ -23,4 +22,4 @@ def compose(sample: Union[int, str]) -> List[Optional[Tuple[ProofNet, DAG]]]:
 
 
 def exhaust():
-    return list(chain.from_iterable(map(lambda i: compose(i), tqdm(range(len(_lassy))))))
+    return list(chain.from_iterable(map(lambda i: compose(i), range(len(_lassy)))))
