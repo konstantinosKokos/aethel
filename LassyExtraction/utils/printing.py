ca952bed1fad4d2fd2d986de2e4172f603ec380f
konstantinosKokos <konstantinos@riseup.net>
2021-04-07 11:31:02 +0200

    default args for symprints

--- LassyExtraction/utils/printing.py
+++ LassyExtraction/utils/printing.py
@@ -21,29 +21,29 @@ _box = '□'
 _diamond = '◊'
 
 
-def print_box(x: str) -> str:
+def print_box(x: str = '') -> str:
     return f'{_box}{superscript(x)}'
 
 
-def print_diamond(x: str) -> str:
+def print_diamond(x: str = '') -> str:
     return f'{_diamond}{superscript(x)}'
 
 
-def cap(x: Any) -> str:
+def cap(x: Any = '') -> str:
     # reflects box intro -- box abs
     return f'▴{superscript(x)}'
 
 
-def cup(x: Any) -> str:
+def cup(x: Any = '') -> str:
     # reflects box elim -- box app
     return f'▾{superscript(x)}'
 
 
-def wedge(x: Any) -> str:
+def wedge(x: Any = '') -> str:
     # reflects diamond intro -- diamond app
     return f'▵{superscript(x)}'
 
 
-def vee(x: Any) -> str:
+def vee(x: Any = '') -> str:
     # reflects diamond elim -- diamond abs
     return f'▿{superscript(x)}'
