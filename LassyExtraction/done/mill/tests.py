176e37e21b5d52c6acdaf0b5e3c3a1c75b6c97d1
konstantinosKokos <konstantinos@riseup.net>
2021-12-08 18:59:45 +0100

    backup

--- LassyExtraction/done/mill/tests.py
+++ LassyExtraction/done/mill/tests.py
@@ -1,15 +1,22 @@
-from .types import Atom, Functor, Proof
+from .types import Atom, Functor, Proof, Diamond
 
 A, B, C, D = [Atom(i) for i in 'ABCD']
 AB = Functor(A, B)
-CD = Functor(C, D)
+CD = Functor(Diamond('β', C), D)
 AB_CD = Functor(AB, CD)
 
 
 F = AB_CD.con('F')
 f = AB.var(0)
 g = CD.var(1)
-x = A.con('x')
 
-Ffx = Proof.apply(Proof.apply(F, f), x)
+aA = Diamond('α', A)
+ax = aA.con('x')
+x = Proof.undiamond('α', ax)
+bx = Proof.diamond('β', x)
+
+
+Ffx = Proof.apply(Proof.apply(F, f), bx)
 assert type(Ffx) == D
+
+
