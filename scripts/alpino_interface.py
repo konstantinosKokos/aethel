ac6bab3c2c5eb818e508b601315bbe90ba16b9ac
konstantinosKokos <konstantinos@riseup.net>
2022-07-18 10:01:09 +0200

    update alpino_interface.py with linearity check

--- scripts/alpino_interface.py
+++ scripts/alpino_interface.py
@@ -3,7 +3,7 @@
 """
 
 from LassyExtraction.transformations import prepare_for_extraction
-from LassyExtraction.extraction import prove, Proof
+from LassyExtraction.extraction import prove_dag, Proof, ExtractionError
 from LassyExtraction.utils.lassy import parse
 
 
@@ -13,5 +13,11 @@ def parse_alpino_file(path: str) -> list[Proof]:
     """
     etree = parse(path)
     name = etree.find('sentence').attrib['sentid']
-    trees = prepare_for_extraction(etree, name)
-    return [prove(tree, next(iter(tree.get_roots())), None, None) for tree in trees]
+    dags = prepare_for_extraction(etree, name)
+    proofs = []
+    for dag in dags:
+        try:
+            proofs.append(prove_dag(dag))
+        except ExtractionError:
+            continue
+    return proofs
